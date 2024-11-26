90e717ea2f3812347a0d10af650e4e03e4532234
MerryArbitrary <elijahjubrey@gmail.com>
2024-11-25 18:44:32 -0800

    Add Main and GamePanel

---
+++ src/main/java/me/merry/Main.java
@@ -0,0 +1,19 @@
+package me.merry;
+
+import me.merry.*;
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame mainWindow = new JFrame("window");
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setSize(400,400);
+        mainWindow.setVisible(true);
+
+        GamePanel gamePanel = new GamePanel();
+        mainWindow.add(gamePanel);
+
+        mainWindow.pack();
+    }
+}
